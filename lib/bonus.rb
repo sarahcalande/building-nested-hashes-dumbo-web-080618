93b816d84e9cbbea08996d84adee427f03450189
scalande@yahoo.com <scalande@yahoo.com>
2018-07-23 15:02:23 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
    }
   }
 epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
   #code your solution here:
 
   
