2973161be7640cc6ef9b82e023d2751647739dac
scalande@yahoo.com <scalande@yahoo.com>
2018-07-23 14:42:19 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,17 +1,17 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => { :name => "Lord Montague"
-      :age => "53"
+      :patriarch => { name: => "Lord Montague"
+      age: => "53"
         #your key/value pairs here
         },
-      :matriarch => { :name => "Lady Montague"
-      :age => "54"
+      :matriarch => { name: => "Lady Montague"
+      age: => "54"
         #your key/value pairs here
         },
-      :hero => { :name => "Romeo",
-      :age => "15",
-      :status => "alive"
+      :hero => { name: => "Romeo",
+      age: => "15",
+      status: => "alive"
         #your key/value pairs here
         },
       :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, {name: "Mercutio", :age "18", :attitude => "hot-headed"}]
