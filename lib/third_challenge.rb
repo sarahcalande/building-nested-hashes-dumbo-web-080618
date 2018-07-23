27e2fca17fc7367a286ddc9b1e270bd94b28d12b
scalande@yahoo.com <scalande@yahoo.com>
2018-07-23 14:50:49 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => { name: "Lord Capulet",
-      age: => "50"
+      age: "50"
         #your key/value pairs here
         },
       :matriarch => { name: "Lady Capulet",
