9cff5acd3daa56bcab60c1dd0a2b100c467f6f4f
scalande@yahoo.com <scalande@yahoo.com>
2018-07-23 14:26:16 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -25,7 +25,9 @@ def third_challenge
       :age => 51
         #your key/value pairs here
         },
-      :heroine => {
+      :heroine => { :name => "Juliet"
+      :age => 15
+      :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
