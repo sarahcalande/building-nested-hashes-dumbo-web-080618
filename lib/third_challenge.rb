6aa69962bbf852488ff8936eb2e01bc9ae10c78f
scalande@yahoo.com <scalande@yahoo.com>
2018-07-23 14:47:52 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,31 +1,31 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => { name: => "Lord Montague"
-      age: => "53"
+      :patriarch => { name: "Lord Montague",
+      age: "53"
         #your key/value pairs here
         },
-      :matriarch => { name: => "Lady Montague"
-      age: => "54"
+      :matriarch => { name:"Lady Montague",
+      age: "54"
         #your key/value pairs here
-        }
-      :hero => { name: => "Romeo",
+        },
+      :hero => { name: "Romeo",
       age: => "15",
       status: => "alive"
         #your key/value pairs here
         },
-      :hero_friends => [{name: => "Benvolio", age: => "17", attitude: => "worried"}, {name: "Mercutio", age: "18", attitude: => "hot-headed"}]
+      :hero_friends => [{name: "Benvolio", age:"17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
    }, 
    :capulet => {
-      :patriarch => { name: => "Lord Capulet",
+      :patriarch => { name: "Lord Capulet",
       age: => "50"
         #your key/value pairs here
         },
-      :matriarch => { name: => "Lady Capulet",
+      :matriarch => { name: "Lady Capulet",
       age: "51"
         #your key/value pairs here
         }
-      :heroine => { name: => "Juliet"
+      :heroine => { name: "Juliet"
       age: "15"
       status: "alive"
         #your key/value pairs here
